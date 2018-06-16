6ea2a04d9caa5cdf61e5db9c4ee9b732d8f7ea07
kamalyadav11 <kyzx11@gmail.com>
2018-06-16 13:52:36 +0530

    display album title on screen

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -4,18 +4,24 @@ import axios from 'axios';
 
 export default class AlbumList extends React.Component {
   state = {
-    albums: [] //album list geeting its initial state as an empty array
+    albums: [] //album list getting its initial state as an empty array
   }
+
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums') //making an http request which is an asynchronous request (that means it will take some time) 
       .then(response => this.setState({albums: response.data})); //Now after THAT some time we get our http response, which gives entire list of album
   }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
     console.log(this.state); //first time album list is rendered as an empty array
     //and after the http response albumlist re-renders again showing us the entire list of album
     return(
       <View>
-        <Text>Album List</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
