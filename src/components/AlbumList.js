2c6fc6d1dc3b8d36dc09aa22f2bc7a9306af8938
kamalyadav11 <kyzx11@gmail.com>
2018-06-16 13:35:57 +0530

    added axios library for fetching http response

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
 
-export default AlbumList = () => {
-  return(
-    <View>
-      <Text>Album List</Text>
-    </View>
-  );
+export default class AlbumList extends React.Component {
+  state = {
+    albums: [] //album list geeting its initial state as an empty array
+  }
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums') //making an http request which is an asynchronous request (that means it will take some time) 
+      .then(response => this.setState({albums: response.data})); //Now after THAT some time we get our http response, which gives entire list of album
+  }
+  render() {
+    console.log(this.state); //first time album list is rendered as an empty array
+    //and after the http response albumlist re-renders again showing us the entire list of album
+    return(
+      <View>
+        <Text>Album List</Text>
+      </View>
+    );
+  }
 }
-
