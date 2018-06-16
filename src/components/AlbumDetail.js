5c55df66d330e90f6be61832c0ff5427babd0c16
kamalyadav11 <kyzx11@gmail.com>
2018-06-16 17:16:19 +0530

    added more styling and imgaes

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-export default AlbumDetail = (props) => {
+export default AlbumDetail = ({ album }) => { //now as we are having multiple props.album, so it's a good idea to remove props and destructure albums
+
+  const { title, artist, thumbnail_image } = album;
+  const {thumbnailStyle, headerContentStyle} = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View>
+          <Image
+           style={thumbnailStyle}
+           source={{ uri: thumbnail_image }} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection> 
     </Card>
   );
 }
+
+const styles = StyleSheet.create({
+  headerContentStyle: {
+    justifyContent: 'space-around'
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  }
+});
